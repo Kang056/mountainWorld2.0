6b2c49b26e5224bff3d38783fcb432912d28049d
Howard <a0926036356@gmail.com>
2022-06-07 23:22:57 +0800

    feature: test3

--- src/app/pages/index/map/map.component.ts
+++ src/app/pages/index/map/map.component.ts
@@ -187,7 +187,7 @@ export class MapComponent implements OnInit, OnDestroy {
       loadAngularFunction: (elevation) => (this.elevationChart(elevation)),
     };
   }
-
+// git3
   ngOnDestroy(): void {
     (window as any).resetOpenStreeMap();
   }
